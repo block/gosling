bbaa188d0cae30efad98d7f32a6c0082d7920ff9
Wes <wesbillman@users.noreply.github.com>
2025-02-13 09:15:25 -0700

    chore: add CI workflow for flutter

--- test/features/chat/chat_page_test.dart
+++ test/features/chat/chat_page_test.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_starter/features/chat/chat_page.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../helpers/widget_helpers.dart';
 
 void main() {
-  testWidgets('should increment counter', (WidgetTester tester) async {
+  testWidgets('should have logo and input field', (WidgetTester tester) async {
     await tester.pumpWidget(
       WidgetHelpers.testableWidget(child: const ChatPage()),
     );
 
-    expect(find.text('0'), findsOneWidget);
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pumpAndSettle();
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(SvgPicture), findsOneWidget);
+    expect(find.byType(TextField), findsOneWidget);
   });
 }
